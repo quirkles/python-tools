f5561591ed9cde99639a60b196f648bba1c19116
quirkles <al.quirk@gmail.com>
2024-10-07 16:04:55 -0400

    remove test apps

--- bin/src/store/__main__.py
+++
@@ -1,4 +0,0 @@
-from bin.src.store.main import main
-
-if __name__ == "__main__":
-    main()
--- bin/src/store/main.py
+++
@@ -1,8 +0,0 @@
-from src.store.arg_parser.arg_parser import arg_parser
-from src.store.Datastore import Datastore
-
-
-def main():
-    args = arg_parser()
-    datastore = Datastore()
-    datastore.process(args.key_name, args.action, args.valueOrVersion)
